e7a0bf952b23dacca53c2ac2e93634f5885fbae5
Rollin Francois <rfrancois21@hotmail.com>
2020-04-24 23:18:11 -0400

    Initial Commit

---
+++ ClassDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClassDemo
+//
+//  Created by Rollin Francois on 4/24/20.
+//  Copyright © 2020 Francois Technology. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
